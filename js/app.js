c2346dc0ded606dd694b2568d60faf09aa8fc3ab
LINCOLN <lincolneulogiohuanca15@gmail.com>
2022-06-28 09:12:09 -0500

    creamos el listener y ademas de una funcion que nos permitira disparar si se haya dado click en el boton de agregar al carrito

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 // creamos las variables
 const carrito = document.getElementById('carrito');
 const listaCursos = document.getElementById('lista-cursos');
+
+//listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+    // dispara cuando se presiona el boton de agregar al carrito
+    cursos.addEventListener('click', comprarcurso);
+}
