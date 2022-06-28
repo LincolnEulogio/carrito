214a5533298ca1e3d0b23746537daa6cc7e5261e
LINCOLN <lincolneulogiohuanca15@gmail.com>
2022-06-28 12:23:43 -0500

    function de imprimir o ver nuestros cursos y guardados ahi en localstorage aunque se recargue la pagina

--- js/app.js
+++ js/app.js
@@ -131,3 +131,28 @@ function obtenerCursosLocalStorage() {
     }
     return cursosLS;
 }
+
+
+// imprime los cursos de localStorage en el carrito
+
+function leerLocalStorage() {
+    let cursosLS;
+
+    cursosLS = obtenerCursosLocalStorage();
+    //console.log(cursosLS);
+
+    cursosLS.forEach(function(curso) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width=100>
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+        <a href="#" class="borrar-curso" data-id="${curso.id}">X</a> 
+        </td>
+        `;
+        listaCursos.appendChild(row);
+    });
+}
